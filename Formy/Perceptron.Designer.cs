0a308616dd913cf1bbe7759b5b1aa410fe909e27
Mateusz Cymerman <mateuszcymerman95@gmail.com>
2020-09-08 23:32:39 +0200

    Form dla algorytmu perceptrona

--- Formy/Perceptron.Designer.cs
+++ Formy/Perceptron.Designer.cs
@@ -1,13 +1,11 @@
-﻿using System.ComponentModel;
-
-namespace SpamSmsLicencjat
+﻿namespace SpamSmsLicencjat.Formy
 {
     partial class Perceptron
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private IContainer components = null;
+        private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -19,7 +17,6 @@ namespace SpamSmsLicencjat
             {
                 components.Dispose();
             }
-
             base.Dispose(disposing);
         }
 
@@ -31,12 +28,131 @@ namespace SpamSmsLicencjat
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelWriteMessage = new System.Windows.Forms.Label();
+            this.labelLog = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonShowDiagrams = new System.Windows.Forms.Button();
+            this.textBoxCheckSpam = new System.Windows.Forms.TextBox();
+            this.buttonCheckSpam = new System.Windows.Forms.Button();
+            this.labelLoading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelWriteMessage
+            // 
+            this.labelWriteMessage.AutoSize = true;
+            this.labelWriteMessage.Location = new System.Drawing.Point(64, 177);
+            this.labelWriteMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelWriteMessage.Name = "labelWriteMessage";
+            this.labelWriteMessage.Size = new System.Drawing.Size(314, 15);
+            this.labelWriteMessage.TabIndex = 15;
+            this.labelWriteMessage.Text = "Wpisz po angielsku wiadomość i sprawdź czy jest spamem";
+            this.labelWriteMessage.Visible = false;
+            // 
+            // labelLog
+            // 
+            this.labelLog.AutoSize = true;
+            this.labelLog.Location = new System.Drawing.Point(765, 177);
+            this.labelLog.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLog.Name = "labelLog";
+            this.labelLog.Size = new System.Drawing.Size(87, 15);
+            this.labelLog.TabIndex = 14;
+            this.labelLog.Text = "Wyniki modelu";
+            this.labelLog.Visible = false;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(765, 227);
+            this.textBoxLog.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(604, 376);
+            this.textBoxLog.TabIndex = 13;
+            this.textBoxLog.Visible = false;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(64, 37);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(203, 84);
+            this.buttonLoad.TabIndex = 12;
+            this.buttonLoad.Text = "Utwórz model";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonShowDiagrams
+            // 
+            this.buttonShowDiagrams.Location = new System.Drawing.Point(784, 37);
+            this.buttonShowDiagrams.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonShowDiagrams.Name = "buttonShowDiagrams";
+            this.buttonShowDiagrams.Size = new System.Drawing.Size(237, 66);
+            this.buttonShowDiagrams.TabIndex = 11;
+            this.buttonShowDiagrams.Text = "Pokaż wykresy";
+            this.buttonShowDiagrams.UseVisualStyleBackColor = true;
+            this.buttonShowDiagrams.Click += new System.EventHandler(this.buttonShowDiagrams_Click);
+            // 
+            // textBoxCheckSpam
+            // 
+            this.textBoxCheckSpam.Location = new System.Drawing.Point(64, 206);
+            this.textBoxCheckSpam.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxCheckSpam.Multiline = true;
+            this.textBoxCheckSpam.Name = "textBoxCheckSpam";
+            this.textBoxCheckSpam.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxCheckSpam.Size = new System.Drawing.Size(450, 161);
+            this.textBoxCheckSpam.TabIndex = 9;
+            // 
+            // buttonCheckSpam
+            // 
+            this.buttonCheckSpam.Location = new System.Drawing.Point(64, 413);
+            this.buttonCheckSpam.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonCheckSpam.Name = "buttonCheckSpam";
+            this.buttonCheckSpam.Size = new System.Drawing.Size(203, 54);
+            this.buttonCheckSpam.TabIndex = 8;
+            this.buttonCheckSpam.Text = "Sprawdź czy spam";
+            this.buttonCheckSpam.UseVisualStyleBackColor = true;
+            this.buttonCheckSpam.Click += new System.EventHandler(this.buttonCheckSpam_Click);
+            // 
+            // labelLoading
+            // 
+            this.labelLoading.AutoSize = true;
+            this.labelLoading.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelLoading.Location = new System.Drawing.Point(422, 72);
+            this.labelLoading.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLoading.Name = "labelLoading";
+            this.labelLoading.Size = new System.Drawing.Size(223, 25);
+            this.labelLoading.TabIndex = 7;
+            this.labelLoading.Text = "Trwa ładowanie...";
+            // 
+            // Perceptron
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1380, 611);
+            this.Controls.Add(this.labelLoading);
+            this.Controls.Add(this.buttonCheckSpam);
+            this.Controls.Add(this.textBoxCheckSpam);
+            this.Controls.Add(this.buttonShowDiagrams);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.labelLog);
+            this.Controls.Add(this.labelWriteMessage);
+            this.Name = "Perceptron";
             this.Text = "Perceptron";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelWriteMessage;
+        private System.Windows.Forms.Label labelLog;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonShowDiagrams;
+        private System.Windows.Forms.TextBox textBoxCheckSpam;
+        private System.Windows.Forms.Button buttonCheckSpam;
+        private System.Windows.Forms.Label labelLoading;
     }
 }
--- Formy/Perceptron.cs
+++ Formy/Perceptron.cs
@@ -1,12 +1,382 @@
-﻿using System.Windows.Forms;
+﻿using Accord.Controls;
+using Deedle;
+using Microsoft.ML;
+using SpamSmsLicencjat.Klasy_pomocnicze;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
-namespace SpamSmsLicencjat
+namespace SpamSmsLicencjat.Formy
 {
     public partial class Perceptron : Form
     {
+
+        private static string dataPath = Path.Combine(Environment.CurrentDirectory, "dane.tsv");
+        private static string stopWordsDataPath = Path.Combine(Environment.CurrentDirectory, "stopwords.txt");
+        private static string dataPathCsv = Path.Combine(Environment.CurrentDirectory, "dane.csv");
+
+        PredictionEngine<SpamInputAveragedPerceptron, SpamPredictionAveragedPerceptron> predictionEngine;
+        private bool _loading = true;
+
         public Perceptron()
         {
             InitializeComponent();
+            ChangeVisibility();
+            labelLoading.Visible = false;
+            buttonLoad.Visible = true;
+        }
+
+        private void StartModel()
+        {
+
+            //machine learning kontekst
+            var context = new MLContext();
+
+            //dane z pliku mapowane na SpamInput
+            var data = context.Data.LoadFromTextFile<SpamInputAveragedPerceptron>(
+                path: dataPath,
+                hasHeader: true,
+                separatorChar: '\t');
+
+            //użycie 80%  danych na trening i 20% do testowania
+            var partitions = context.Data.TrainTestSplit(
+                data,
+                testFraction: 0.2);
+
+            // transofrmacja tekstu
+            var dataProcessPipeline = context.Transforms.Conversion.MapValueToKey("Label", "Label")
+                .Append(context.Transforms.Text.FeaturizeText("FeaturesText",
+                    new Microsoft.ML.Transforms.Text.TextFeaturizingEstimator.Options
+                    {
+                        WordFeatureExtractor = new Microsoft.ML.Transforms.Text.WordBagEstimator.Options
+                        { NgramLength = 2, UseAllLengths = true },
+                        CharFeatureExtractor = new Microsoft.ML.Transforms.Text.WordBagEstimator.Options
+                        { NgramLength = 3, UseAllLengths = false },
+                    }, "Message"))
+                .Append(context.Transforms.CopyColumns("Features", "FeaturesText"))
+                .Append(context.Transforms.NormalizeLpNorm("Features", "Features"))
+                .AppendCacheCheckpoint(context);
+
+            // ustaw aalgorytm
+            var trainer = context.MulticlassClassification.Trainers.OneVersusAll(
+                    context.BinaryClassification.Trainers.AveragedPerceptron(labelColumnName: "Label",
+                        numberOfIterations: 10, featureColumnName: "Features"), labelColumnName: "Label")
+                .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel", "PredictedLabel"));
+            var trainingPipeLine = dataProcessPipeline.Append(trainer);
+
+            var crossValidationResults =
+                context.MulticlassClassification.CrossValidate(data: data, estimator: trainingPipeLine,
+                    numberOfFolds: 5);
+
+            StringBuilder sb = new StringBuilder();
+
+            //raport wyniku - sprawdzamy czy nie ma zbyt wielu szczegołów (ale jest ok , bo wcześniej wyknaliśmy NLP w kroku 2
+            foreach (var r in crossValidationResults)
+            {
+                sb.Append(
+                    $"  Średni AUC - czyli jak model jest dokładny (skala mikro): {crossValidationResults.Average(r => r.Metrics.MicroAccuracy)} \n");
+                sb.Append(
+                    $"  Średni AUC - czyli jak model jest dokładny (skala makro): {crossValidationResults.Average(r => r.Metrics.MacroAccuracy)} \n");
+            }
+
+            //trenujemy model na zbiorze treningowym
+            Console.WriteLine("Trenowanie modelu ...");
+            var model = trainingPipeLine.Fit(partitions.TrainSet);
+
+            // ocena modelu na zbiorze testowym
+            Console.WriteLine("Ocena modelu ....");
+            var predictions = model.Transform(partitions.TestSet);
+            var metrics = context.MulticlassClassification.Evaluate(
+                data: predictions,
+                labelColumnName: "Label",
+                scoreColumnName: "Score");
+
+            // raport wynik
+            sb.Append($"  Dokładność makro:          {metrics.MacroAccuracy:P2} \n");
+            sb.Append($"  Dokładność mikro:          {metrics.MicroAccuracy:P2} \n");
+            sb.Append($"  Jak dużo błędów (0 = brak , 1 = same błędy:           {metrics.LogLoss:0.##} \n");
+            sb.Append($"  LogLossReduction:  {metrics.LogLossReduction:0.##} \n");
+            sb.Append(metrics.ConfusionMatrix.GetFormattedConfusionTable());
+
+
+            PredictionEngine<SpamInputAveragedPerceptron, SpamPredictionAveragedPerceptron> predictionEngine =
+                context.Model.CreatePredictionEngine<SpamInputAveragedPerceptron, SpamPredictionAveragedPerceptron>(
+                    model);
+
+            textBoxLog.Text = sb.ToString();
+
+            _loading = false;
+            ChangeVisibility();
+        }
+
+        private void Start()
+        {
+            _loading = true;
+            ChangeVisibility();
+            MessageBox.Show("Trwa ładowanie", "Ładowanie");
+
+            StartModel();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void buttonCheckSpam_Click(object sender, EventArgs e)
+        {
+            var inputUser = textBoxCheckSpam.Text;
+
+            var message = new SpamInputAveragedPerceptron();
+            message.Message = inputUser;
+
+
+            // utworzenie predykcji
+            var prediction = predictionEngine.Predict(message);
+
+            // pokazanie wyniku
+            MessageBox.Show($"Wiadomość '{inputUser}' jest { (prediction.isSpam == "spam" ? "spam" : "not spam" )}", "Wynik");
+            textBoxCheckSpam.Clear();
+        }
+
+        private void buttonShowDiagrams_Click(object sender, EventArgs e)
+        {
+            _loading = true;
+            ChangeVisibility();
+            MessageBox.Show("Trwa ładowanie", "Ładowanie");
+
+            // wczytanie danych z pliku csv
+            var przetworzoneDaneZPlikuCsv = Frame.ReadCsv(
+                dataPathCsv,
+                hasHeaders: true,
+                inferTypes: false,
+                schema: "int,string"
+            );
+
+            // sprawdzenie slow 
+            var wiadomoscWordVecDf = MakeWordVec(przetworzoneDaneZPlikuCsv.GetColumn<string>("wiadomosc"));
+            wiadomoscWordVecDf.SaveCsv(Path.Combine(Environment.CurrentDirectory, "tekst_wordvec_tylko_litery.csv"));
+
+            // wczytanie stopwordsow z pliku tekstowego
+            ISet<string> stopWords = new HashSet<string>(
+                File.ReadLines(stopWordsDataPath)
+            );
+
+            var hamSmsCount = przetworzoneDaneZPlikuCsv.GetColumn<int>("czy_ham").NumSum();
+            var spamSmsCount = wiadomoscWordVecDf.RowCount - hamSmsCount;
+
+            wiadomoscWordVecDf.AddColumn("czy_ham", przetworzoneDaneZPlikuCsv.GetColumn<int>("czy_ham"));
+            var hamTermFrequencies = wiadomoscWordVecDf.Where(
+                x => x.Value.GetAs<int>("czy_ham") == 1
+            ).Sum().Sort().Reversed.Where(x => x.Key != "czy_ham");
+
+            var spamTermFrequencies = wiadomoscWordVecDf.Where(
+                x => x.Value.GetAs<int>("czy_ham") == 0
+            ).Sum().Sort().Reversed;
+
+            var hamTermProportions = hamTermFrequencies / hamSmsCount;
+            var topHamTerms = hamTermProportions.Keys.Take(10);
+            var topHamTermsProportions = hamTermProportions.Values.Take(10);
+
+            File.WriteAllLines(
+               Path.Combine(Environment.CurrentDirectory, "ham-czestotliwosc.csv"),
+                hamTermFrequencies.Keys.Zip(
+                    hamTermFrequencies.Values, (a, b) => string.Format("{0},{1}", a, b)
+                )
+            );
+
+            var spamTermProportions = spamTermFrequencies / spamSmsCount;
+            var topSpamTerms = spamTermProportions.Keys.Take(10);
+            var topSpamTermsProportions = spamTermProportions.Values.Take(10);
+
+            File.WriteAllLines(
+                Path.Combine(Environment.CurrentDirectory, "spam-czestotliwosc.csv"),
+                spamTermFrequencies.Keys.Zip(
+                    spamTermFrequencies.Values, (a, b) => string.Format("{0},{1}", a, b)
+                )
+            );
+
+            var barChart = DataBarBox.Show(
+                new string[] { "Ham", "Spam" },
+                new double[] {
+                    hamSmsCount,
+                    spamSmsCount
+                }
+            );
+            barChart.SetTitle("Ham vs. Spam ogólny rozkład");
+
+            var hamBarChart = DataBarBox.Show(
+                topHamTerms.ToArray(),
+                new double[][] {
+                    topHamTermsProportions.ToArray(),
+                    spamTermProportions.GetItems(topHamTerms).Values.ToArray()
+                }
+            );
+            hamBarChart.AutoScaleMode = AutoScaleMode.Inherit;
+            hamBarChart.SetTitle("Top używanych 10 słów w Ham Smsach przed filtrowaniem stopwordsow (niebieski: HAM, czerwony: SPAM)");
+            System.Threading.Thread.Sleep(3000);
+            hamBarChart.Invoke(
+                new Action(() =>
+                {
+                    hamBarChart.Size = new Size(2000, 1500);
+                })
+            );
+
+            var spamBarChart = DataBarBox.Show(
+                topSpamTerms.ToArray(),
+                new double[][] {
+                    hamTermProportions.GetItems(topSpamTerms).Values.ToArray(),
+                    topSpamTermsProportions.ToArray()
+                }
+            );
+
+            spamBarChart.AutoScaleMode = AutoScaleMode.Inherit;
+            spamBarChart.SetTitle("Top 10 używanych słów w Spam Smsach przed filtrowaniem stopwordsów(niebieskie: HAM, czerwony: SPAM)");
+            System.Threading.Thread.Sleep(2000);
+            spamBarChart.Invoke(
+                new Action(() =>
+                {
+                    spamBarChart.Size = new Size(2000, 1500);
+                })
+            );
+
+
+            var hamTermFrequenciesAfterStopWords = hamTermFrequencies.Where(
+                x => !stopWords.Contains(x.Key)
+            );
+            var hamTermProportionsAfterStopWords = hamTermProportions.Where(
+                x => !stopWords.Contains(x.Key)
+            );
+            var topHamTermsAfterStopWords = hamTermProportionsAfterStopWords.Keys.Take(10);
+            var topHamTermsProportionsAfterStopWords = hamTermProportionsAfterStopWords.Values.Take(10);
+            File.WriteAllLines(
+                Path.Combine(Environment.CurrentDirectory, "ham-czestotliwosc-po-przefiltrowaniu-stopwordsow.csv"),
+                hamTermFrequenciesAfterStopWords.Keys.Zip(
+                    hamTermFrequenciesAfterStopWords.Values, (a, b) => string.Format($"{a},{b}")
+                )
+            );
+
+            var spamTermFrequenciesAfterStopWords = spamTermFrequencies.Where(
+                x => !stopWords.Contains(x.Key)
+            );
+            var spamTermProportionsAfterStopWords = spamTermProportions.Where(
+                x => !stopWords.Contains(x.Key)
+            );
+            var topSpamTermsAfterStopWords = spamTermProportionsAfterStopWords.Keys.Take(10);
+            var topSpamTermsProportionsAfterStopWords = spamTermProportionsAfterStopWords.Values.Take(10);
+            File.WriteAllLines(
+                 Path.Combine(Environment.CurrentDirectory, "spam-czestotliwosc-po-przefiltrowaniu-stopwordsow.csv"),
+                spamTermFrequenciesAfterStopWords.Keys.Zip(
+                    spamTermFrequenciesAfterStopWords.Values, (a, b) => string.Format($"{a},{b}")
+                )
+            );
+
+            hamBarChart = DataBarBox.Show(
+                topHamTermsAfterStopWords.ToArray(),
+                new double[][] {
+                    topHamTermsProportionsAfterStopWords.ToArray(),
+                    spamTermProportionsAfterStopWords.GetItems(topHamTermsAfterStopWords).Values.ToArray()
+                }
+            );
+            hamBarChart.SetTitle("Top 10 słów w Ham smsach - po filtrowaniu stopwordsów (niebieski: HAM, czerwony: SPAM)");
+            hamBarChart.AutoScaleMode = AutoScaleMode.Inherit;
+            System.Threading.Thread.Sleep(3000);
+
+            hamBarChart.Invoke(
+                new Action(() =>
+                {
+                    hamBarChart.Size = new Size(2000, 1500);
+                })
+            );
+
+            spamBarChart = DataBarBox.Show(
+                topSpamTermsAfterStopWords.ToArray(),
+                new double[][] {
+                    hamTermProportionsAfterStopWords.GetItems(topSpamTermsAfterStopWords).Values.ToArray(),
+                    topSpamTermsProportionsAfterStopWords.ToArray()
+                }
+            );
+            spamBarChart.SetTitle("Top 10 słów w Spam smsach - po wyfiltrowaniu stopwordsow (niebieski: HAM, czerwony: SPAM)");
+            spamBarChart.AutoScaleMode = AutoScaleMode.Inherit;
+            System.Threading.Thread.Sleep(3000);
+
+            spamBarChart.Invoke(
+                new Action(() =>
+                {
+                    spamBarChart.Size = new Size(2000, 1500);
+                })
+            );
+
+            _loading = false;
+            ChangeVisibility();
+        }
+
+        private static Frame<int, string> MakeWordVec(Series<int, string> rows)
+        {
+            var wordsByRows = rows.GetAllValues().Select((x, i) =>
+            {
+                var sb = new SeriesBuilder<string, int>();
+
+                ISet<string> words = new HashSet<string>(
+                    Regex.Matches(
+                        // same litery
+                        x.Value, "[a-zA-Z]+('(s|d|t|ve|m))?"
+                    ).Cast<Match>().Select(
+                        // zamiana na male litery
+                        y => y.Value.ToLower()
+                    ).ToArray()
+                );
+
+                // zamiana slowa ktore wystepuja w kazdym wierszu na 1
+                foreach (string w in words)
+                {
+                    sb.Add(w, 1);
+                }
+
+                return KeyValue.Create(i, sb.Series);
+            });
+
+            // brakujace zamieniam na 0
+            var wordVecDF = Frame.FromRows(wordsByRows).FillMissing(0);
+
+            return wordVecDF;
+        }
+
+        private void ChangeVisibility()
+        {
+            if (_loading)
+            {
+                labelLoading.Visible = true;
+
+                buttonCheckSpam.Visible = false;
+                textBoxCheckSpam.Visible = false;
+                buttonShowDiagrams.Visible = false;
+                buttonLoad.Visible = false;
+
+                labelWriteMessage.Visible = false;
+                labelLog.Visible = false;
+                textBoxLog.Visible = false;
+            }
+            else
+            {
+                labelLoading.Visible = false;
+
+                buttonCheckSpam.Visible = true;
+                textBoxCheckSpam.Visible = true;
+                buttonShowDiagrams.Visible = true;
+
+                buttonLoad.Visible = false;
+
+                labelWriteMessage.Visible = true;
+                labelLog.Visible = true;
+                textBoxLog.Visible = true;
+            }
         }
     }
 }
